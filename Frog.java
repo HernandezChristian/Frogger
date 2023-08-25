374477204b308a337f0feaf5aa6f3f0fa53f2e2e
HernandezChristian <142824138+HernandezChristian@users.noreply.github.com>
2023-08-25 16:22:43 +0000

    constructor

--- Frog.java
+++ Frog.java
@@ -2,7 +2,10 @@ import.objectdraw.*;
 import.java.awt*;
 public class frog{
 private VisibleImage frogImage;
-
+private staticfinal double FROG_HEIGHT = 48;
+  public frog(Image i, DrawingCanvas c){
+    frogImage = new visibeImage(i, new Location(10,10), c);
+  }
 
   
 }

3ba4ee3d36bce60b78675b98da8ea9a65ab7ac96
SimplyKoi <142824414+SimplyKoi@users.noreply.github.com>
2023-08-25 16:25:21 +0000

    Push me :)

--- Frog.java
+++ Frog.java
@@ -6,6 +6,8 @@ private staticfinal double FROG_HEIGHT = 48;
   public frog(Image i, DrawingCanvas c){
     frogImage = new visibeImage(i, new Location(10,10), c);
   }
-
+public boolean overlaps(VisibleImage vehicleImage){
+  
+}
   
 }

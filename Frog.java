b421250a33de4f04f0fec2221de659c6a0773f3a
SimplyKoi <142824414+SimplyKoi@users.noreply.github.com>
2023-08-29 16:37:33 +0000

    Bug fix 1

--- Frog.java
+++ Frog.java
@@ -1,8 +1,8 @@
-import.objectdraw.*;
-import.java.awt*;
+import objectdraw.*;
+import java.awt.*;
 public class frog{
 private VisibleImage frogImage;
-private staticfinal double FROG_HEIGHT = 48;
+private static final double FROG_HEIGHT = 48;
   public frog(Image i, DrawingCanvas c){
     frogImage = new visibeImage(i, new Location(10,10), c);
   }

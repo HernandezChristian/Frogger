8099e17b82f98cb643ff70d96dc549ab928f88d3
HernandezChristian <142824138+HernandezChristian@users.noreply.github.com>
2023-09-11 16:44:53 +0000

    frog works

--- Frog.java
+++ Frog.java
@@ -4,7 +4,7 @@ public class Frog{
 private VisibleImage frogImage;
 private static final double FROG_HEIGHT = 48;
   public Frog(Image i, DrawingCanvas c){
-    frogImage = new VisibeImage(i, new Location(10,10), c);
+    frogImage = new VisibleImage(i, new Location(10,10), c);
   }
 public boolean overlaps(VisibleImage vehicleImage){
   return frogImage.overlaps(vehicleImage);

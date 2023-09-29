220bcf3dba223a30df3afe6cc2f5ff970ad9f637
HernandezChristian <142824138+HernandezChristian@users.noreply.github.com>
2023-09-29 16:24:23 +0000

    dead image

--- Frog.java
+++ Frog.java
@@ -8,7 +8,7 @@ private boolean isAlive;
 private static final double FROG_HEIGHT = 48;
 private int lives;
 private int score;
-  public Image dead;
+  public VisibleImage dead;
   private DrawingCanvas can;
   public Frog(Image i, DrawingCanvas c){
     frogImage = new VisibleImage(i, new Location(350,350), c);
@@ -16,7 +16,8 @@ private int score;
     can = c;
     lives = 3;
     score = 0;
-    dead
+    dead = new VisibleImage(getImage("deadfrog.png"),new Location(0,0),c);
+    dead.hide();
   }
 public boolean overlaps(VisibleImage vehicleImage){
   //return frogImage.overlaps(vehicleImage);
@@ -58,6 +59,8 @@ public void hop(String direction){
 	}
   public void kill(){
     frogImage.hide();
+    dead.moveTo(frogImage.getX(), frogImage.getY());
+    dead.show();
     //can.getGraphics().drawString("OUCH", 350,300);
     isAlive = false;
     lives--;

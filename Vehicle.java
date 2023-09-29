800cd16868d9efeb4431b3c658177817ed932b8a
SimplyKoi <142824414+SimplyKoi@users.noreply.github.com>
2023-09-29 16:34:07 +0000

    car overlap

--- Vehicle.java
+++ Vehicle.java
@@ -5,31 +5,33 @@ public class Vehicle extends ActiveObject{
   private VisibleImage carImage;
  private double carSpeed;
   boolean alive = true;
+  private Frog juan;
 
-  public Vehicle(int carX, int carY, Image i, double speed, DrawingCanvas c) {
+  public Vehicle(int carX, int carY, Image i, double speed, DrawingCanvas c, Frog x) {
 
     carImage = new VisibleImage(i, new Location(carX, carY), c);
 carSpeed = speed;
+    juan = x;
     this.start();
   }
 
   public void run() {
    
 //Loop for car to run
-    double rate = -1;
-    double time = 5;
+  
+    
 
 while( carImage.getX() < 1500 && carImage.getX() > -100 && alive){
 
   long time = System.currentTimeMillis(); 
    pause(20.0);
     long Actualtime = System.currentTimeMillis() - time; 
-      double distance = (carSpeed* time);
- carImage.move(distance, 0);
-  if  (juan.overlaps(carImage, true))
-  {
-    alive = false;
-  }
+      double distance = (carSpeed* Actualtime);
+   carImage.move(distance, 0);
+    if  (juan.overlaps(carImage, true))
+    {
+      alive = false;
+    }
 
    }
 }
